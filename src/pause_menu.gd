a7c43ce209fd0bbe47f041ff3551c9b632cb6352
DragonAxe <12703200+DragonAxe@users.noreply.github.com>
2024-09-06 15:19:48 -0600

    Add initial Godot 4.3 project
    Includes my pause menu code and an addon to add controller/keyboard icons for player controls

---
+++ src/pause_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@export_category("Scene Internal Nodes")
+@export var dialog: Control
+@export var focused_control: Control
+
+
+func _ready() -> void:
+  dialog.visible = false
+
+
+func _input(event: InputEvent) -> void:
+  if get_tree().paused:
+    if event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause_unpause"):
+      resume()
+  else:
+    if event.is_action_pressed("pause_unpause"):
+      pause()
+    
+
+
+func resume() -> void:
+  get_tree().paused = false
+  dialog.visible = false
+
+
+func pause() -> void:
+  focused_control.grab_focus()
+  get_tree().paused = true
+  dialog.visible = true
---
+++ src/quit.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func quit() -> void:
+  get_tree().quit()
