2fa49fe5ca6725b051d9f68f08f844f9983850d4
Robin Campos <magyk81@gmail.com>
2024-09-07 13:11:25 -0600

    no changes at all

--- src/golem.gd
+++ src/golem.gd
@@ -1,10 +1,26 @@
 extends RigidBody2D
 
-@export_category("Internal Nodes")
-@export var integrate_forces_modules: Array[Node]
+@export_category("Internal nodes")
+@export var navigation_agent: NavigationAgent2D
+
+
+func _ready() -> void:
+  set_movement_target.call_deferred(global_position)
+  var _ignore: int = navigation_agent.target_reached.connect(target_reached)
+
+
+func _process(delta: float) -> void:
+  pass
 
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-  for module: Node in integrate_forces_modules:
-    assert(module.has_method("integrate_forces"))
-    module.call("integrate_forces", state)
+  pass
+
+
+func set_movement_target(movement_target: Vector2) -> void:
+  await get_tree().physics_frame
+  navigation_agent.target_position = movement_target
+
+
+func target_reached() -> void:
+  pass
