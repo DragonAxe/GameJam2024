103b14ec6cb3157da01ffc917b3845cee84efa8a
Robin Campos <magyk81@gmail.com>
2024-09-07 17:28:40 -0600

    Start lerp rotation

--- src/grab_controls.gd
+++ src/grab_controls.gd
@@ -1,20 +1,22 @@
 extends Node
 
 @export var grab_area : Area2D
+@export var map : Sprite2D
 
 func _input(event: InputEvent) -> void:
   if event.is_action_pressed("player_grab_release"):
     # Check whether the player is already holding an obelisk.
-    var already_holding : bool = false
+    var obelisk_held : Obelisk = null
     for child : Node in grab_area.get_children():
       if child is Obelisk:
-        already_holding = true
+        obelisk_held = child as Obelisk
         break
     
-    if already_holding:
-      # Check that the obelisk is not overlapping a wall or enemy and that it's inside the map.
-      
-      pass
+    if obelisk_held:
+      var obelisk_position : Vector2 = obelisk_held.global_position
+      grab_area.remove_child(obelisk_held)
+      get_parent().get_parent().add_child(obelisk_held)
+      obelisk_held.position = obelisk_position
     else:
       # Check that the obelisk is very close to the player in the direction that they're facing.
       var obelisk_areas : Array[Area2D] = grab_area.get_overlapping_areas()
--- src/movement_controls.gd
+++ src/movement_controls.gd
@@ -1,8 +1,14 @@
 extends Node
 
+@onready var parent: RigidBody2D = get_parent()
 
 func integrate_forces(state: PhysicsDirectBodyState2D) -> void:
   var player_vector: Vector2 = Input.get_vector(
     "player_left", "player_right", "player_up", "player_down"
   )
   state.linear_velocity = player_vector * 210
+
+  # Face player in the direction of where it's moving.
+  var angle_to_target : float = player_vector.angle_to(Vector2.UP.rotated(parent.rotation))
+  parent.rotate(angle_to_target * 0.1)
+  # parent.look_at(parent.global_position + player_vector.rotated(PI / 2))
