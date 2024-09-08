13a82e26bf8b01993b8994644943c195a1bc5b1e
DragonAxe <12703200+DragonAxe@users.noreply.github.com>
2024-09-08 17:26:43 -0600

    Add WinUI and moved obelisks

--- src/win_ui.gd
+++ src/win_ui.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 
 func _process(delta: float) -> void:
-  var obelisks: Array[Node] = get_tree().get_nodes_in_group("hag_stone")
+  var obelisks: Array[Node] = get_tree().get_nodes_in_group("obelisk")
   var completed_count: int = 0
   for obelisk: Obelisk in obelisks:
     if obelisk.completed:
