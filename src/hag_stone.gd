fff06ac3b9f153cbb202eb785c86277240d1adb5
DragonAxe <12703200+DragonAxe@users.noreply.github.com>
2024-09-08 00:51:37 -0600

    Add hag stones that capture obelisks placed on top of them

---
+++ src/hag_stone.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export_category("Internal nodes")
+@export var snap_area: Area2D
+
+var captured: int = 0
+
+
+func _physics_process(delta: float) -> void:
+  for area: Area2D in snap_area.get_overlapping_areas():
+    if area.get_parent() is Obelisk:
+      var obelisk: Obelisk = area.get_parent()
+      if area.get_parent() != self:
+        if captured == 0:
+          captured = 30
+          capture_obelisk.call_deferred(obelisk)
+  if len(snap_area.get_overlapping_areas()) == 0:
+    if captured > 0:
+      captured -= 1
+
+
+
+func capture_obelisk(obelisk: Obelisk) -> void:
+  obelisk.get_parent().remove_child(obelisk)
+  add_child(obelisk)
+  obelisk.global_position = global_position
